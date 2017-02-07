8c4fea2316631b1b5f49b91fdc73bdfd9b239d15
Mark Andrew Gerads <nazgand@gmail.com>
2017-02-06 22:18:45 -0600

    Create Lnmtl-Learn-Chinese.user.js

---
+++ scripts/Lnmtl-Learn-Chinese.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Lnmtl Learn Chinese
+// @namespace    http://lnmtl.com/chapter/
+// @version      0.1
+// @description  show the 字 next to the words that would usually be in popups
+// @author       Nazgand
+// @match        http://lnmtl.com/chapter/*
+// @grant        none
+// ==/UserScript==
+(function () {
+  document.querySelector('button.js-toggle-original').click();
+  document.querySelectorAll('.translated [data-original-title]').forEach(
+    function (e) {
+      e.innerText = '{' + e.getAttribute('data-title') + e.innerText + '}';
+    });
+})();
