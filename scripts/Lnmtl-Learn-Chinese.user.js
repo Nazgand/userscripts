8c738b6ae2f8a162e5ab0c3ad94a3cf5b67f95c0
Mark Andrew Gerads <nazgand@gmail.com>
2017-02-10 05:40:59 -0600

    word wrap fix

--- scripts/Lnmtl-Learn-Chinese.user.js
+++ scripts/Lnmtl-Learn-Chinese.user.js
@@ -1,17 +1,15 @@
 // ==UserScript==
 // @name         Lnmtl Learn Chinese
 // @namespace    http://lnmtl.com/chapter/
-// @version      0.1
+// @version      0.2
 // @description  show the 字 next to the words that would usually be in popups
 // @author       Nazgand
 // @match        http://lnmtl.com/chapter/*
 // @grant        none
 // ==/UserScript==
-(function() {
-  document.querySelector('button.js-toggle-original').click();
-  document.querySelectorAll('.translated [data-original-title]').forEach(
-    function(e) {
-      e.innerText = '{' + e.getAttribute('data-title') + ':' + e.innerText
-        + '}';
-    });
-})();
+document.querySelector('button.js-toggle-original').click();
+document.querySelectorAll('.translated [data-original-title]').forEach(
+  function (e) {
+    e.innerText = '{' + e.getAttribute('data-title') + ':' + e.innerText + '}';
+    e.style.whiteSpace='nowrap';
+  });
