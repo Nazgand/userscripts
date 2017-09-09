c99243e0d74c2f62565d6c9c50f52d00ebbea5ae
Mark Andrew Gerads <nazgand@gmail.com>
2017-09-09 13:26:09 +0000

    Create Qidian-Adwall-Defeater.user.js

---
+++ scripts/Qidian-Adwall-Defeater.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Qidian Adwall Defeater
+// @namespace    whatever
+// @version      1
+// @description  passes adwall
+// @author       <nazgand@gmail.com>
+// @match        https://www.webnovel.com/book/*
+// @match        https://www.webnovel.com/rssbook/*
+// @grant        none
+// @homepage     https://github.com/nazgand/userscripts
+// ==/UserScript==
+
+//Suggested use: with Google Chrome AutoMute Extension
+
+function autoClickBook(){
+    const btnPlay=document.querySelector('a.bt._play');
+    if (btnPlay!==null) btnPlay.click();
+
+    const btnSkip=document.querySelector('a.bt._skip.j_can_skip');
+    if (btnSkip!==null) btnSkip.click();
+
+    const btnCheckIn=document.querySelector('a.fr._bt.j_check_in');
+    if (btnCheckIn.innerHTML=='Check-in') btnCheckIn.click();
+
+    const divLockVideo=document.querySelector('div.lock-video');
+    const pContinued=document.querySelector('p.cha-ft[id="continued"]');
+    if (pContinued===null || divLockVideo!==null)
+        setTimeout(autoClickBook,1000);
+}
+if (document.location.href.startsWith('https://www.webnovel.com/book/'))
+    autoClickBook();
+
+function autoClickRSSBook(){
+    var btnRead=document.querySelector('a.bt.j_read[disabled]');
+    if (btnRead!==null) {
+        setTimeout(autoClickRSSBook,500);
+    } else {
+        btnRead=document.querySelector('a.bt.j_read');
+        btnRead.click();
+    }
+}
+if (document.location.href.startsWith('https://www.webnovel.com/rssbook/'))
+    autoClickRSSBook();
