16bf3e8c295e39e48866b0027b1bb120ff795839
Mark Andrew Gerads <nazgand@gmail.com>
2018-04-30 17:23:04 -0500

    Simple Google Plus Default Audience Checker
    Whenever Google+ has the default audience something other than what you want, this script will remind you to choose your audience.

---
+++ scripts/Simple-Google-Plus-Default-Audience-Checker.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name     Simple Google Plus Default Audience Checker
+// @namespace  http://tampermonkey.net/
+// @version    0.1
+// @description  Whenever Google+ has the default audience something other than what you want, this script will remind you to choose your audience.
+// @author     Nazgand
+// @match    https://plus.google.com/
+// @grant    none
+// ==/UserScript==
+
+//constants to minimize errors made by script
+const svgCommunity = "M18";
+const svgPublic = "M24 4";
+const svgCircle = "M33";
+const svgPerson = "M32";
+const svgCollection = "M24 2";
+
+
+// BEGIN Changable constants ----------------------------------------
+const defaultAudienceNameDesired = 'Public';
+const defaultAudienceSvgDesired = svgPublic;
+const frequencyToCheckForNewPost = 500;
+const frequencyToCheckForButton = 50;
+const patience = 4400;
+// END   Changable constants ----------------------------------------
+
+
+var lastContentPublishingBox = 0, beganWaiting=0;
+function waitForAndPress(){//Find the Audience Change button
+  var shareWithButton=lastContentPublishingBox.querySelector('span[role="button"][aria-label^="Share with"]');
+  //wait for the options to load, yet do not wait too long
+  if(shareWithButton===null || Date.now()>beganWaiting+patience){
+    setTimeout(waitForAndPress, frequencyToCheckForButton);
+  } else {
+    //Check to see whether change is needed while considering communities named 'Public'
+    if(shareWithButton.attributes['aria-label'].value!=='Share with: '+defaultAudienceNameDesired ||
+     !shareWithButton.innerHTML.startsWith(defaultAudienceNameDesired) ||
+     !shareWithButton.querySelector('svg>path').attributes.d.value.startsWith(defaultAudienceSvgDesired)) {
+      //click button to remind of need to change Audience
+      shareWithButton.click();
+    }
+  }
+}
+function main(){
+  //check for new ContentPublishingBox
+  var newContentPublishingBox = document.querySelector('c-wiz[role="dialog"]:not([aria-hidden])>c-wiz[data-av]>content');
+  if(newContentPublishingBox!==null && lastContentPublishingBox!==newContentPublishingBox){
+    lastContentPublishingBox = newContentPublishingBox;
+    beganWaiting = Date.now();
+    waitForAndPress();
+  }
+  setTimeout(main, frequencyToCheckForNewPost);
+}
+main();
